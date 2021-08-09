40b5575e08bc58a3341745832dfa496f36d8f103
andrea-artuso <ultrandr3x@gmail.com>
2021-08-09 19:01:16 +0200

    Created header and search input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { SearchIcon } from '@heroicons/react/solid'
+import { useState } from 'react';
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className="website-header">
+        <div className="input-container">
+          <input type="text" onChange={e=>setSearchInput(e.target.value)} placeholder="Enter search keywords" aria-label="Enter your search words" />
+          <button onClick={()=>console.log(searchInput)} className="search-button" aria-label="Search button">
+            <SearchIcon 
+              width="20px"
+              height="20px"
+            />
+          </button>
+        </div>
+      </header>
     </div>
   );
 }
